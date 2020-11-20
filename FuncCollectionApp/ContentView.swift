c333a0f7835df2df9bc6a15c6aee04f9d35f0fc1
HyunSBong <sanseng@mju.ac.kr>
2020-11-20 20:19:39 +0900

    Initial Commit

---
+++ FuncCollectionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FuncCollectionApp
+//
+//  Created by Hyunsu Bong on 2020/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
