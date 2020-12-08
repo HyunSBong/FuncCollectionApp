4630888c8b7fada70f0f45449241f5aa97397e9a
HyunSBong <sanseng@mju.ac.kr>
2020-12-08 20:10:30 +0900

    addView(nuil)

---
+++ FuncCollectionApp/Preview Content/Example04View.swift
@@ -0,0 +1,20 @@
+//
+//  Example04View.swift
+//  FuncCollectionApp
+//
+//  Created by Hyunsu Bong on 2020/12/08.
+//
+
+import SwiftUI
+
+struct Example04View: View {
+    var body: some View {
+        Text("준비중")
+    }
+}
+
+struct Example04View_Previews: PreviewProvider {
+    static var previews: some View {
+        Example04View()
+    }
+}
---
+++ FuncCollectionApp/Preview Content/Example05View.swift
@@ -0,0 +1,20 @@
+//
+//  Example05View.swift
+//  FuncCollectionApp
+//
+//  Created by Hyunsu Bong on 2020/12/08.
+//
+
+import SwiftUI
+
+struct Example05View: View {
+    var body: some View {
+        Text("준비중")
+    }
+}
+
+struct Example05View_Previews: PreviewProvider {
+    static var previews: some View {
+        Example05View()
+    }
+}
