0b5f7cbf48733be1a6af97f5e04a4dd014c809ad
HyunSBong <sanseng@mju.ac.kr>
2020-12-13 18:30:20 +0900

    토글, 사용자 추가 구현

--- FuncCollectionApp/Preview Content/Example04View.swift
+++ FuncCollectionApp/Preview Content/Example04View.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Hyunsu Bong on 2020/12/12.
 //
-
 import SwiftUI
 
 struct UserView: View { // 사용자 추가 뷰
@@ -23,19 +22,41 @@ struct UserView: View { // 사용자 추가 뷰
         }
     }
 }
+//아직 사용하지 않음
+/*
+struct AlertView: View {
+    @Binding var showUserView: Bool
+
+    var body: some View {
+        NavigationView {
+            Text("알림 설정 화면입니다.")
+            .navigationBarTitle(Text("알림 형태"), displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {
+                    print("consol : 완료버튼 눌림")
+                    self.showUserView = false
+                }) {
+                    Text("완료").bold()
+                })
+        }
+    }
+}
+*/
 
 struct Example04View: View {
     @State var username: String = ""
     @State var password: String = ""
     @State var isAuto: Bool = true
     @State var notifivationEnabled: Bool = false
+    @State private var bannerIndex = 0
     @State private var previewIndex = 0
+    @State private var alertIndex = 0
     @State var showUserView = false // 사용자 추가 버튼 변수
-    var previewOptions = ["항상", "사용자 설정"]
     
+    var bannerStyle = ["일시적 표시", "지속적 표시"]
+    var previewOptions = ["항상", "잠겨있지 않을 때(기본)", "안 함"]
+    var alertGroup = ["자동", "앱별로 정리", "끔"]
     
     var body: some View {
-//        NavigationView {
             Form {
                 Section(header: Text("프로파일")) {
                     TextField("UserName", text: $username)
@@ -48,9 +69,21 @@ struct Example04View: View {
                     Toggle(isOn: $notifivationEnabled) {
                         Text("알림 허용")
                     }
-                    Picker(selection: $previewIndex, label: Text("알림 형태")) {
+                    Picker(selection: $bannerIndex, label: Text("배너 스타일")) {
+                        ForEach(0 ..< bannerStyle.count) {
+                            Text(self.bannerStyle[$0])
+                        }
+                    }
+                }
+                Section(header: Text("옵션")) {
+                    Picker(selection: $previewIndex, label: Text("미리보기 표시")) {
                         ForEach(0 ..< previewOptions.count) {
                             Text(self.previewOptions[$0])
+                        }
+                    }
+                    Picker(selection: $alertIndex, label: Text("알림 그룹 설정")) {
+                        ForEach(0 ..< alertGroup.count) {
+                            Text(self.alertGroup[$0])
                         }
                     }
                 }
@@ -70,18 +103,6 @@ struct Example04View: View {
                     }
                 }
             }
-//            .navigationBarItems(trailing:
-//                    Button(action: {
-//                        self.showSheetView.toggle()
-//                        print("consol : 네비게이션 바 우측 버튼 눌림")
-//                    }) {
-//                        Image(systemName: "bell.circle.fill")
-//                            .font(Font.system(.title))
-//                    }
-//                ) // 네비게이션 바 우측에 버튼 추가
-//        }.sheet(isPresented: $showUserView) {
-//            SheetView(showUserView: self.$showUserView)
-//        }
             
         .navigationBarTitle(Text("설정"), displayMode: .inline)
             
@@ -91,7 +112,7 @@ struct Example04View: View {
                 self.showUserView.toggle()
                 print("consol : 네비게이션 바 우측 버튼 눌림")
             }) {
-                Image(systemName: "bell.circle.fill")
+                Image(systemName: "person.crop.circle.badge.plus")
                     .font(Font.system(.title))
             }
         ) // 네비게이션 바 우측에 버튼 추가
