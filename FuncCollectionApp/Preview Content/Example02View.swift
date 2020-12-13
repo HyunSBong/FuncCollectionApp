599d307e4417d75355e5eaa0bb4aae12cb24b659
HyunSBong <sanseng@mju.ac.kr>
2020-12-13 18:30:51 +0900

    사진 불러오기 일부 구현 (View만)

--- FuncCollectionApp/Preview Content/Example02View.swift
+++ FuncCollectionApp/Preview Content/Example02View.swift
@@ -7,14 +7,44 @@
 
 import SwiftUI
 import Combine
+import UIKit
 // 이미지 프레임 참고
 //https://www.simpleswiftguide.com/swiftui-image-tutorial/
 
+struct photoView: View {
+    @Binding var nameList: String
+    @Binding var imgList: String
+    @Binding var showPictureView: Bool
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("이름입력", text: $nameList)
+                    Button(action: {
+                        print("consol : 리셋버튼 눌림")
+                    }) {
+                        Text("사진 가져오기")
+                    }
+                }
+            .navigationBarTitle(Text("알림 형태"), displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {
+                    print("consol : 완료버튼 눌림")
+                    self.showPictureView = false
+                }) {
+                    Text("완료").bold()
+                })
+            }
+        }
+    }
+}
+
 struct Example02View: View {
     
+    @State var showPictureView = false // 객체 추가 버튼 변수
     @State private var name : String = ""
-    var nameList = ["Cat", "Dog", "PONGKI", "Hyuji"]
-    var imgList = ["IMG_0078", "IMG_1405", "IMG_1405", "IMG_0078"]
+    @State var nameList = ["Cat", "Dog", "PONGKI", "Hyuji"]
+    @State var imgList = ["IMG_0078", "IMG_1405", "IMG_1405", "IMG_0078"]
     
     @State private var selected = 0
     
@@ -24,8 +54,6 @@ struct Example02View: View {
             VStack {
                 //타이틀
 //                Spacer().frame(height: 30)
-                Text("2. Picker 구현하기")
-                    .font(.title)
                 
                 // Picker 만들기
                 Picker(selection: $selected, label: Text("동물")) {
@@ -38,10 +66,25 @@ struct Example02View: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 350.0, height: 350.0, alignment: .center)
-            }
+            }.navigationBarTitle(Text("2. Picker 구현하기"), displayMode: .inline)
             
         }.onAppear(){
         }.padding(.all, 16.0)
+        .navigationBarTitle(Text("설정"), displayMode: .inline)
+            
+        // 사용자 추가 버튼
+        .navigationBarItems(trailing:
+            Button(action: {
+                self.showPictureView.toggle()
+                print("consol : 네비게이션 바 우측 버튼 눌림")
+            }) {
+                Image(systemName: "plus.circle.fill")
+                    .font(Font.system(.title))
+            }
+        ) // 네비게이션 바 우측에 버튼 추가
+            .sheet(isPresented: $showPictureView) {
+                SheetView(showSheetView: self.$showPictureView)
+            }
     }
 }
 
